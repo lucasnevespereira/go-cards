3a63ee478b6450ac1f031c05a2379cbdada41908
lucas.nevespereira <lucas.nevespereira@eemi.com>
2020-07-27 20:38:39 +0200

    declaring variables

--- main.go
+++ main.go
@@ -1,6 +1,14 @@
 // Reminder: Package main defines an executable output
 package main
 
+import "fmt"
+
 func main() {
 
+	// var card string = "Ace of Spades"
+	// or
+	card := "Ace of Spades" // go compilers will figure out that card is a string thanks to := operator, only when creating variable
+	card = "Five of diamonds"
+
+	fmt.Println(card)
 }
